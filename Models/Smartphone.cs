3fb9c79eea7fd2d70fda413ffa4677aa291df3cd
caio_angerame <caio.angerame2@gmail.com>
2025-09-29 13:51:08 -0300

    TODOs Implementados
    Implementação dos TODOs solicitados para o desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,21 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public string Modelo { get; set; }
+
+        public string IMEI { get; set; }
+
+        public int Memoria { get; set; }
+        // Implementado: Implementar as propriedades faltantes de acordo com o diagrama
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            // Implementado: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero: "12345678", modelo: "Nokia", imei: "72647828", memoria: 128);
+Console.WriteLine($"Numero: {nokia.Numero} \nModelo: {nokia.Modelo} \nIMEI: {nokia.IMEI} \nMemória: {nokia.Memoria}");
+nokia.Ligar();
+nokia.InstalarAplicativo("Twitter");
+
+Console.WriteLine("\nSmartphone Iphone:");
+Iphone iphone = new Iphone(numero: "12345678", modelo: "Iphone", imei: "72647828", memoria: 128);
+Console.WriteLine($"Numero: {iphone.Numero} \nModelo: {iphone.Modelo} \nIMEI: {iphone.IMEI} \nMemória: {iphone.Memoria}");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Twitter");
